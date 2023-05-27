6590d11cd555c5a3defd416317da16ed48497a54
Emilio Gil Berbegal <emilio.gil.berbegal@gmail.com>
2023-05-28 01:19:10 +0200

    Configuración dev service worker

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(),
+  VitePWA({
+    injectRegister: 'auto',
+    registerType: 'autoUpdate',
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    devOptions: {
+      enabled: true
+    }
+  })
+  ]
 });

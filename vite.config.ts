25a4b9d1c909b2e80809e0a91ac43e60ff435d4a
Emilio Gil Berbegal <emilio.gil.berbegal@gmail.com>
2023-06-05 18:52:17 +0200

    PWA instalable

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,37 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(),
-  VitePWA({
-    injectRegister: 'auto',
-    registerType: 'autoUpdate',
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-    },
-  })
-  ]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["img/logo.png"],
+      manifest: {
+        name: "My Awesome App",
+        short_name: "MyApp",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "img/logo.png",
+            sizes: "225x225",
+            type: "image/png",
+          },
+          {
+            src: "img/logo.png",
+            sizes: "225x225",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
